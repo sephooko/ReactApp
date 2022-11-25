8c5beeed728fd560c89c59af7c81fccc50e5bc60
Malwina <92362873+sephooko@users.noreply.github.com>
2022-11-25 10:49:16 +0100

    Update config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAx-_vGnB1MKjKHFZibNrE6Z6JQp50OX3s",
+  authDomain: "fir-authapp-2e4ac.firebaseapp.com",
+  projectId: "fir-authapp-2e4ac",
+  storageBucket: "fir-authapp-2e4ac.appspot.com",
+  messagingSenderId: "721496852862",
+  appId: "1:721496852862:web:7dbadb3195888043944b2f"
+};
